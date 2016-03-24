d8d1c24f1674e0e0da9346e74bc755b07e0e043e
Bryden Arndt <bryden@arndt.ca>
2016-03-24 12:59:21 -0400

    added loop for multi-databases

--- bud.sh
+++ bud.sh
@@ -7,7 +7,7 @@ USER="root" # a mysql admin user account
 PASS="root" # the mysql admin password
 BACKDIR="/home/brydena/mysql-bak" # the directory where you setup your mysql-bak folder
 FULLBACKUPLIFE=4320 # how often a full backup (rather than incremental) should be completed (4320 == 3 days)
-DATABASE="" # the database that should be targeted for back-up
+DATABASES=("vtigercrm" "vtigercrm540") # the databases that should be targeted for back-up, ("db1" "db2" "db3")
 EMAIL="" # your email address for success/failure notifications
 #NOTIFY_SUCCESS="YES" # notify on success? YES/NO
 
@@ -18,13 +18,6 @@ REMOTEUSER="" # the username that you have setup for ssh
 REMOTEHOST="" # the hostname of the server to ssh into
 REMOTEDIR="/home/arndtb/bud/" # the remote directory to copy backups to
 
-#################################
-# SYSTEM SETTINGS - DO NOT EDIT #
-#################################
-BASEBACKDIR="$BACKDIR/$DATABASE" # leave as-is
-#INCRBACKDIR="$BACKDIR/incr" # leave as-is
-START=`date +%s` # leave as-is
-TMPFILE="$DATABASE-$START.sql" # leave as-is
 
 ########
 # CODE #
@@ -53,34 +46,46 @@ echo "BUD: Datadir listed in MySQL as: $DATADIR"
 echo "BUD: Setup check completed fantastically. Pat yourself on the back and tell yourself \"Good work!\""
 echo "BUD: Back to more serious things. Checking for latest backup file..."
 
-# CHECK FOR LATEST BACKUP
-LATEST=`find $BASEBACKDIR -mindepth 1 -maxdepth 1 -printf "%P\n" | sort -nr | head -1`
+# Make a backup of each database
+for db in "${DATABASES[@]}"
+do
+    #################################
+    # SYSTEM SETTINGS - DO NOT EDIT #
+    #################################
+    BASEBACKDIR="$BACKDIR/$db" # leave as-is
+    #INCRBACKDIR="$BACKDIR/incr" # leave as-is
+    START=`date +%s` # leave as-is
+    TMPFILE="$db-$START.sql" # leave as-is
 
-AGE=`stat -c %Y $BASEBACKDIR/$LATEST`
-#echo "AGE : $AGE\n LATEST : $LATEST"
 
-#if [ "$LATEST" -a `expr $AGE + $FULLBACKUPLIFE + 5` -ge $START ]
-#then
+    # CHECK FOR LATEST BACKUP
+    LATEST=`find $BASEBACKDIR -mindepth 1 -maxdepth 1 -printf "%P\n" | sort -nr | head -1`
 
-#else
-    echo "BUD: New full backup"
-    # Create a new full backup
-    echo "BUD: Saving to $BASEBACKDIR/$TMPFILE"
-    mysqldump -u $USER --password=$PASS $DATABASE > $BASEBACKDIR/$TMPFILE
-    tar -zcvf $BASEBACKDIR/$TMPFILE.tar.gz $BASEBACKDIR/$TMPFILE
-    rm $BASEBACKDIR/$TMPFILE
-    TMPFILE="$TMPFILE.tar.gz"
+    AGE=`stat -c %Y $BASEBACKDIR/$LATEST`
+    #echo "AGE : $AGE\n LATEST : $LATEST"
 
-    # Email notification to admin
-    MESSAGE="Full backup of MySQL completed successfully for $DATABASE.\n"
-    MESSAGE="$MESSAGE $BASEBACKDIR/$TMPFILE\n"
-    echo $MESSAGE | mail -s "BUD: Full backup successful" "$EMAIL"
+    #if [ "$LATEST" -a `expr $AGE + $FULLBACKUPLIFE + 5` -ge $START ]
+    #then
 
-    # Copy the database to a remote folder
-    echo "BUD: Transferring $TMPFILE to $REMOTEHOST:/$REMOTEDIR"
-    scp $BASEBACKDIR/$TMPFILE $REMOTEUSER@$REMOTEHOST:/$REMOTEDIR/
-    echo "BUD: Removing last backup: $LATEST"
-    rm $BASEBACKDIR/$LATEST
-    echo 'BUD: Exiting. Goodbye.'
-#fi
+    #else
+        echo "BUD: New full backup"
+        # Create a new full backup
+        echo "BUD: Saving to $BASEBACKDIR/$TMPFILE"
+        mysqldump -u $USER --password=$PASS $db > $BASEBACKDIR/$TMPFILE
+        tar -zcvf $BASEBACKDIR/$TMPFILE.tar.gz $BASEBACKDIR/$TMPFILE
+        rm $BASEBACKDIR/$TMPFILE
+        TMPFILE="$TMPFILE.tar.gz"
+    
+        # Email notification to admin
+        MESSAGE="Full backup of MySQL completed successfully for $db.\n"
+        MESSAGE="$MESSAGE $BASEBACKDIR/$TMPFILE\n"
+        echo $MESSAGE | mail -s "BUD: Full backup successful" "$EMAIL"
 
+        # Copy the database to a remote folder
+        echo "BUD: Transferring $TMPFILE to $REMOTEHOST:/$REMOTEDIR"
+        scp $BASEBACKDIR/$TMPFILE $REMOTEUSER@$REMOTEHOST:/$REMOTEDIR/
+        echo "BUD: Removing last backup: $LATEST"
+        rm $BASEBACKDIR/$LATEST
+        echo 'BUD: Exiting. Goodbye.'
+    #fi
+done
